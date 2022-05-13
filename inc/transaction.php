d7ee64c5cc9974beead9aa0576a0dd7a3afff750
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-05-13 22:09:18 +0700

    [FE-WU] Membuat tampilan transaksi

---
+++ inc/transaction.php
@@ -0,0 +1,31 @@
+<div class="container-fluid">
+	<div class="container">
+		<div class="row justify-content-center px-3 px-md-4 my-4">
+			<div class="card border-0 border-top-2 col-md-11">
+		  		<div class="row justify-content-end">
+		  			<div class="col-2 col-sm-2 col-lg-1">
+		  				<span class="ribbonTestimoni"><i class="fas fa-file-invoice fa-2x"></i></span>
+		  			</div>
+		  			<div class="col-10 col-sm-10 col-lg-11">
+
+					<div class="card-body">
+						<h1 class="text-success">Detail Transaksi</h1>
+					</div>
+				</div>
+		  	</div>
+
+		  	<div class="col-11 border-dotted-5"></div>
+		</div>
+	</div>
+</div>
+
+<div class="container my-5">
+	<div class="row justify-content-center px-3 px-md-4">
+	  	<div class="card col-md-11 bg-white text-dark rounded-100 border-top-7 shadow p-2 p-md-4 my-2">
+	  	</div>
+	</div>
+
+	<div class="row justify-content-center my-4">
+		<div class="col-11 border-dotted-5"></div>
+	</div>
+</div>
