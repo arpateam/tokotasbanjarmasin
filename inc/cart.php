5ce4c60a9c5637f94e87d6548ee83ca64a31d575
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-05-13 22:06:42 +0700

    $ git commit -m

---
+++ inc/cart.php
@@ -0,0 +1,38 @@
+<div class="container-fluid">
+	<div class="container">
+		<div class="row justify-content-center px-3 px-md-4 my-4">
+			<div class="card border-0 border-top-2 col-md-11">
+		  		<div class="row justify-content-end">
+		  			<div class="col-2 col-sm-2 col-lg-1">
+		  				<span class="ribbonTestimoni"><i class="fas fa-shopping-cart fa-2x"></i></span>
+		  			</div>
+		  			<div class="col-10 col-sm-10 col-lg-11">
+
+					<div class="card-body">
+						<h1 class="text-success">Keranjang Belanjaan Saya</h1>
+					</div>
+				</div>
+		  	</div>
+
+		  	<div class="col-11 border-dotted-5"></div>
+		</div>
+	</div>
+</div>
+
+<div class="container my-5">
+	<div class="row justify-content-center px-3 px-md-4">
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+  			<div class="row justify-content-center">
+  				<div class="col-10 col-sm-8 col-lg-6 text-center">
+  					<img src="assets/images/empty_cart.png" alt="Keranjang Saya Kosong" class="img-fluid">
+  					<h2 class="text-warning">Yahhh! Keranjang Anda Masih Kosong</h2>
+  					<a href="produk/" class="btn btn-lg btn-success my-4">Yuk Belanja <i class="fas fa-cart-plus"></i></a>
+  				</div>
+  			</div>
+	  	</div>
+	</div>
+
+	<div class="row justify-content-center my-4">
+		<div class="col-11 border-dotted-5"></div>
+	</div>
+</div>
