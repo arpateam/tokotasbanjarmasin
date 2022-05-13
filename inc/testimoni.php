c8800cda734b1f3198a2ea82b056bbe38527d153
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-05-13 22:04:35 +0700

    [FE-WU] Membuat tampilan testimoni

---
+++ inc/testimoni.php
@@ -0,0 +1,218 @@
+<div class="container-fluid">
+	<div class="container">
+		<div class="row justify-content-center px-3 px-md-4 my-4">
+			<div class="card border-0 border-top-2 col-md-11">
+		  		<div class="row justify-content-end">
+		  			<div class="col-2 col-sm-2 col-lg-1">
+		  				<span class="ribbonTestimoni"><i class="fas fa-star fa-2x"></i></span>
+		  			</div>
+		  			<div class="col-10 col-sm-10 col-lg-11">
+
+					<div class="card-body">
+						<h1 class="text-success">Testimoni Pelanggan Kami</h1>
+					</div>
+				</div>
+		  	</div>
+
+		  	<div class="col-11 border-dotted-5"></div>
+		</div>
+	</div>
+</div>
+
+<div class="container my-5">
+	<div class="row justify-content-center px-3 px-md-4">
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-lg-2 border-end border-success text-success text-center">
+	  				<h1>14</h1>
+	  				<h6>Desember</h6>
+	  				<h4>2021</h4>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-lg-10">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Testimoni Pak Hendy</h3>
+				  		<h4 class="text-success d-block d-md-none">Testimoni Pak Hendy</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h5>
+
+				  		<h6 class="d-block d-md-none">Cepat jg ya. Dah diterima. Pempek adaan paling ok. Lain kali cobain pempek kapal selam dehc. ”Z</h6>
+			  		</div>
+	  			</div>
+		  	</div>
+	  	</div>
+	</div>
+
+	<div class="row justify-content-center my-4">
+		<div class="col-11 border-dotted-5"></div>
+	</div>
+</div>
