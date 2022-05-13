a574101527c18c60f6ed243af0f189c137a2b474
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-05-13 22:02:48 +0700

    [FE-WU] Membuat tampilan home/beranda

---
+++ inc/home.php
@@ -0,0 +1,170 @@
+<div class="hero-wrapper">
+    <figure>
+        <img src="https://tokotasbanjarmasin.com/assets/parallax-banner/banner-beranda-toko-tas-banjarmasin-20220512-125258.png" alt="Banner Beranda <?= $nama_web ?>">
+        <div class="button-parallax d-none d-md-block">
+        	<h3 class="text-success">Toko Tas Muthmainnah</h3>
+        	<p class="text-muted">Toko Grosir Tas terlengkap di daerah Banjarmasin. Menyediakan berbagai macam Tas Ransel, Tas Slempang, Tas Anak, Koper, dll.</p>
+        	<a href="produk/" role="button" class="btn btn-success"><i class="fas fa-cart-plus"></i> Yuk Berbelanja</a>
+        </div>
+
+        <div class="button-parallax d-block d-md-none">
+        	<h5 class="text-success">Toko Tas Muthmainnah</h5>
+        	<small class="text-muted fw-light mb-2">Toko Grosir Tas terlengkap di daerah Banjarmasin. Menyediakan berbagai macam Tas Ransel, Tas Slempang, Tas Anak, Koper, dll.</small>
+        	<a href="produk/" role="button" class="btn btn-sm btn-success mt-2"><i class="fas fa-cart-plus"></i> Yuk Berbelanja</a>
+        </div>
+    </figure>
+</div>
+
+<div class="container my-5">
+
+	<div class="row justify-content-center my-4">
+		<div class="col-11">
+			<h1 class="text-success">Produk Promo</h1>
+		</div>
+		<div class="col-11 border-dotted-5"></div>
+	</div>
+
+	<div class="row justify-content-center px-3 px-md-4">
+
+	  	<div class="col-md-6 col-lg-4 my-4">
+		    <a href="produk/test-prosuk.html" class="card shadow-sm h-100 text-decoration-none produk-hover">
+		    	<div class='ribbon ribbon-top-left'><span>Disc. 51%</span></div>
+		      	<img src="https://tokotasbanjarmasin.com/assets/images/produk/jims-honey--wendy-bag-tas-wanita-selempang-dan-handbag-92.png" class="card-img-top" alt="Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag">
+		      	<div class="card-body">
+		        	<h4 class="card-title text-success">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag</h4>
+		        	<small class="text-muted">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag Introducing Wendy Bag from Jims Honey...</small>
+		      	</div>
+	      		<div class="ribbonHarga">
+		        	<h6 class="text-muted"><del>Rp350.000</del> <span class="text-danger">Disc. 51%</span></h6>
+		        	<h4 class="text-success"><i class="fas fa-tag"></i> Rp171.500</h4>
+		        </div>
+		    </a>
+	  	</div>
+
+	  	<div class="col-md-6 col-lg-4 my-4">
+		    <a href="produk/test-prosuk.html" class="card shadow-sm h-100 text-decoration-none produk-hover">
+		    	<div class='ribbon ribbon-top-left'><span>Disc. 51%</span></div>
+		      	<img src="https://tokotasbanjarmasin.com/assets/images/produk/jims-honey--wendy-bag-tas-wanita-selempang-dan-handbag-92.png" class="card-img-top" alt="Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag">
+		      	<div class="card-body">
+		        	<h4 class="card-title text-success">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag</h4>
+		        	<small class="text-muted">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag Introducing Wendy Bag from Jims Honey...</small>
+		      	</div>
+	      		<div class="ribbonHarga">
+		        	<h6 class="text-muted"><del>Rp350.000</del> <span class="text-danger">Disc. 51%</span></h6>
+		        	<h4 class="text-success"><i class="fas fa-tag"></i> Rp171.500</h4>
+		        </div>
+		    </a>
+	  	</div>
+
+	  	<div class="col-md-6 col-lg-4 my-4">
+		    <a href="produk/test-prosuk.html" class="card shadow-sm h-100 text-decoration-none produk-hover">
+		    	<div class='ribbon ribbon-top-left'><span>Disc. 51%</span></div>
+		      	<img src="https://tokotasbanjarmasin.com/assets/images/produk/jims-honey--wendy-bag-tas-wanita-selempang-dan-handbag-92.png" class="card-img-top" alt="Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag">
+		      	<div class="card-body">
+		        	<h4 class="card-title text-success">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag</h4>
+		        	<small class="text-muted">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag Introducing Wendy Bag from Jims Honey...</small>
+		      	</div>
+	      		<div class="ribbonHarga">
+		        	<h6 class="text-muted"><del>Rp350.000</del> <span class="text-danger">Disc. 51%</span></h6>
+		        	<h4 class="text-success"><i class="fas fa-tag"></i> Rp171.500</h4>
+		        </div>
+		    </a>
+	  	</div>
+
+	  	<div class="w-100"></div>
+
+	  	<div class="col-auto my-4">
+	  		<a role="button" href="promo/" title="Selengkapnya" class="btn btn-success">Selengkapnya <i class="fas fa-arrow-right"></i></a>
+	  	</div>
+	</div>
+
+	<div class="row justify-content-center my-4">
+		<div class="col-11">
+			<h1 class="text-success">Produk Terbaru</h1>
+		</div>
+		<div class="col-11 border-dotted-5"></div>
+	</div>
+
+	<div class="row justify-content-center px-3 px-md-4">
+	  	<div class="col-md-6 col-lg-4 my-4">
+		    <a href="produk/test-prosuk.html" class="card shadow-sm h-100 text-decoration-none produk-hover">
+		      	<img src="https://tokotasbanjarmasin.com/assets/images/produk/tas-backpack-wanita-multifungsi-raindoz-73.png" class="card-img-top" alt="Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag">
+		      	<div class="card-body">
+		        	<h4 class="card-title text-success">Tas Backpack Wanita Multifungsi Raindoz</h4>
+		        	<small class="text-muted">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag Introducing Wendy Bag from Jims Honey...</small>
+		      	</div>
+	      		<div class="ribbonHarga">
+		        	<h4 class="text-success"><i class="fas fa-tag"></i> Rp132.132</h4>
+		      	</div>
+		    </a>
+	  	</div>
+	  	<div class="col-md-6 col-lg-4 my-4">
+		    <a href="produk/test-prosuk.html" class="card shadow-sm h-100 text-decoration-none produk-hover">
+		      	<img src="https://tokotasbanjarmasin.com/assets/images/produk/tas-backpack-wanita-multifungsi-raindoz-73.png" class="card-img-top" alt="Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag">
+		      	<div class="card-body">
+		        	<h4 class="card-title text-success">Tas Backpack Wanita Multifungsi Raindoz</h4>
+		        	<small class="text-muted">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag Introducing Wendy Bag from Jims Honey...</small>
+		      	</div>
+	      		<div class="ribbonHarga">
+		        	<h4 class="text-success"><i class="fas fa-tag"></i> Rp132.132</h4>
+		      	</div>
+		    </a>
+	  	</div>
+	  	<div class="col-md-6 col-lg-4 my-4">
+		    <a href="produk/test-prosuk.html" class="card shadow-sm h-100 text-decoration-none produk-hover">
+		      	<img src="https://tokotasbanjarmasin.com/assets/images/produk/tas-backpack-wanita-multifungsi-raindoz-73.png" class="card-img-top" alt="Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag">
+		      	<div class="card-body">
+		        	<h4 class="card-title text-success">Tas Backpack Wanita Multifungsi Raindoz</h4>
+		        	<small class="text-muted">Jims Honey - Wendy Bag Tas Wanita Selempang Dan Handbag Introducing Wendy Bag from Jims Honey...</small>
+		      	</div>
+	      		<div class="ribbonHarga">
+		        	<h4 class="text-success"><i class="fas fa-tag"></i> Rp132.132</h4>
+		      	</div>
+		    </a>
+	  	</div>
+
+	  	<div class="w-100"></div>
+
+	  	<div class="col-auto my-4">
+	  		<a role="button" href="produk/" title="Selengkapnya" class="btn btn-success">Selengkapnya <i class="fas fa-arrow-right"></i></a>
+	  	</div>
+	</div>
+
+	<div class="row justify-content-center my-4">
+		<div class="col-11 border-dotted-5"></div>
+	</div>
+
+	<div class="row justify-content-center px-3 px-md-4">
+	  	<div class="card col-md-11 bg-white rounded-3 shadow p-3 p-md-5 my-4">
+	  		<div class="row justify-content-end">
+	  			<div class="col-3 col-sm-2 col-md-1">
+	  				<span class="ribbonTestimoni"><i class="fas fa-star fa-lg"></i></span>
+	  			</div>
+	  			<div class="col-9 col-sm-10 col-md-11">
+		            <div class="card-title">
+				  		<h3 class="text-success d-none d-md-block">Dengarkan Testimoni Pelanggan Kami</h3>
+				  		<h4 class="text-success d-block d-md-none">Dengarkan Testimoni Pelanggan Kami</h4>
+		            </div>
+
+			  		<div class="card-body">
+				  		<h5 class="d-none d-md-block">Ini mah beda ma yg di Jakarta. Bener2 asli Palembang. Enak uey!</h5>
+				  		<h6 class="text-muted d-none d-md-block"><i class="fas fa-long-arrow-alt-right"></i> Testimoni Pak Hendy</h6>
+
+				  		<h6 class="d-block d-md-none">Ini mah beda ma yg di Jakarta. Bener2 asli Palembang. Enak uey!</h6>
+				  		<span class="text-muted d-block d-md-none"><i class="fas fa-long-arrow-alt-right"></i> Testimoni Pak Hendy</span>
+			  		</div>
+	  			</div>
+		  	</div>
+
+	  		<div class="card-footer bg-transparent">
+		  		<div class="row justify-content-end">
+		  			<a href="testimoni" role="button" class="col-8 col-sm-6 col-md-4 col-xl-3 btn btn-success">Selengkapnya <i class="fas fa-arrow-right"></i></a>
+		  		</div>
+	  		</div>
+	  	</div>
+	</div>
+
+	<div class="row justify-content-center my-4">
+		<div class="col-11 border-dotted-5"></div>
+	</div>
+</div>
