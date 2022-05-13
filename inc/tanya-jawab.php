dac791e13e5161df999fb084142b46776bc55b91
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-05-13 22:56:53 +0700

    [BE-WU] Sistem FAQ

--- inc/tanya-jawab.php
+++ inc/tanya-jawab.php
@@ -21,16 +21,12 @@
 
 <div class="container my-5">
 	<div class="row justify-content-center px-3 px-md-4">
+    	<?php
+            $Data 		= $pdo->query("SELECT deskripsi FROM page WHERE id_page='3'");
+            $resultData = $Data->fetch(PDO::FETCH_ASSOC)
+        ?>
 	  	<div class="card col-md-11 bg-white rounded-100 border-start-7 shadow p-2 p-md-4 my-2">
-	  		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consectetur in dolor vel aliquam. Curabitur auctor dictum sodales. Phasellus ullamcorper dignissim justo nec suscipit. Duis auctor suscipit nisi in elementum. Maecenas nunc massa, iaculis id nunc ac, vestibulum hendrerit leo. Maecenas imperdiet hendrerit massa, nec pellentesque urna egestas vitae. Quisque varius viverra imperdiet. Fusce eleifend sodales nulla vitae consequat. Nunc auctor magna semper hendrerit malesuada. Etiam vulputate velit eget quam mollis placerat. Sed mollis orci nibh, quis ullamcorper massa sodales eget.</p>
-
-	  		<p>Curabitur posuere arcu at metus molestie semper. Nunc venenatis lorem quis elit elementum, quis fringilla quam luctus. Quisque ultricies neque vel enim cursus, bibendum tincidunt arcu consequat. Mauris consequat ipsum ac cursus dapibus. Cras et dignissim quam. Cras est sapien, varius eu odio ultricies, pellentesque maximus mi. Aenean et elit vitae risus aliquet vehicula volutpat et est. Suspendisse purus mi, accumsan sed quam in, finibus luctus purus. Cras viverra imperdiet mi, ultrices sollicitudin elit ullamcorper sed. Nullam tristique ut elit eget pharetra.</p>
-
-	  		<p>Suspendisse pharetra arcu eros. Fusce faucibus vel magna ut vestibulum. Aliquam sed tortor in magna porttitor elementum. Vestibulum pellentesque elementum nulla eget facilisis. Aenean congue pellentesque mi vitae vehicula. Sed metus ipsum, condimentum eget dui quis, pretium ultricies justo. Nunc eget lectus scelerisque, aliquam ligula a, pellentesque leo. Proin venenatis dignissim nibh vel elementum. Duis quam ipsum, tincidunt vel dignissim eu, venenatis a massa. Pellentesque imperdiet erat ac felis pharetra dignissim. Aliquam luctus eget lacus dictum venenatis.</p>
-
-	  		<p>Maecenas ullamcorper ligula et elementum blandit. Nam volutpat pellentesque semper. Interdum et malesuada fames ac ante ipsum primis in faucibus. In hac habitasse platea dictumst. Sed feugiat dui ante, vel luctus orci accumsan non. Morbi orci tortor, posuere eget massa ac, accumsan rutrum elit. Cras ornare felis sed semper ornare. Donec molestie ipsum risus, euismod hendrerit massa posuere a.</p>
-
-	  		<p>Vivamus quis urna nisi. In pellentesque ullamcorper commodo. Suspendisse eros quam, hendrerit sit amet libero vel, ornare consequat dui. Mauris tincidunt, est quis rutrum viverra, neque ipsum tempor mi, quis accumsan arcu magna sed odio. Nunc vulputate urna risus, id feugiat metus tincidunt nec. Vestibulum non bibendum nibh, eget mattis purus. Cras et purus in erat laoreet vehicula ac in tellus. Morbi nec molestie lorem. Morbi tempor rutrum hendrerit. In hac habitasse platea dictumst. Morbi tellus justo, accumsan vel dictum et, vestibulum nec neque. Mauris luctus ipsum in fermentum lobortis. Donec a pharetra nisi. Nullam venenatis leo scelerisque ornare tincidunt.</p>
+	  		<?= $resultData['deskripsi'] ?>
 	  	</div>
 	</div>
 
