8f18a55b84613bf2b0c905f46c4881d3fbcb51c1
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2022-05-13 22:02:15 +0700

    [FE-WU] Membuat tampilan header menu & footer

---
+++ inc/footerV2.php
@@ -0,0 +1,41 @@
+<footer class="py-4 bg-success border-top-danger-5">
+    <div class="container">
+        <div class="row px-0 px-md-4">
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <h5 class="text-light border-bottom-3 pb-2 mb-4">Tentang Kami</h5>
+                <div class="text-light">
+                    <h5><strong>Toko Tas Muthmainnah</strong></h5>
+                    <p style="text-align: justify;">Tempatnya Toko Tas terlengkap di Banjarmasin. Kami juga menerima Grosir Tas terlengkap dengan berbagai macam Tas Ransel, Tas Slempang, Tas Anak, Koper, dll.</p>
+                </div>
+            </div>
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <h5 class="text-light border-bottom-3 pb-2 mb-4">Kontak Kami</h5>
+
+                <div class="text-light">
+                    Jalan Kelud Industri No. 30, Bambe, Kec Lakarsantri, Kota Surabaya, Jawa Timur 61177
+                </div>
+
+                <hr />
+
+                <h5 class="text-light border-bottom-3 pb-2">Ubah Bahasa Ke</h5>
+
+                <div id="google_translate_element"></div>
+            </div>
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <h5 class="text-light border-bottom-3 pb-2 mb-4">Maps</h5>
+                <div class="text-light">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109564.36134857162!2d106.79056161415582!3d-6.246483069503118!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f3b3ce74daf9%3A0xa98c55fba89ad824!2sFAMILY%20RESTO!5e1!3m2!1sid!2sid!4v1639823176599!5m2!1sid!2sid" width="100%" height="250" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<footer class="py-3 bg-danger">
+    <div class="container">
+        <div class="row px-0 px-md-4 justify-content-between">
+            <div class="col-12 text-center pt-2">
+                <small class="text-light">Copyright &copy; Toko Tas Muthmainnah 2021-<?= date("Y") ?>. All right reserved.</small>
+            </div>
+        </div>
+    </div>
+</footer>
---
+++ inc/headerV2.php
@@ -0,0 +1,48 @@
+<header class="container">
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-10 col-md-5 col-xl-4 text-center">
+            <img src="https://tokotasbanjarmasin.com/assets/images/logo-family-food-versi-desktop.png" alt="Logo <?= $namaweb; ?>" class="img-fluid">
+        </div>
+    </div>
+</header>
+
+<nav class="navbar sticky-top navbar-expand-md navbar-dark bg-success-navbar-dark shadow-sm py-0 py-md-3">
+    <div class="container">
+        <a class="navbar-brand d-block d-md-none" href="/">
+          <img src="https://tokotasbanjarmasin.com/assets/images/logo-family-food-versi-desktop.png" alt="Logo <?= $namaweb; ?>" height="50">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        	<hr class="border-danger d-block d-md-none">
+            <ul class="navbar-nav mx-auto">
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" aria-current="page" href="../">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" href="../produk/">Produk</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" href="../testimoni">Testimoni</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" href="../faq">FAQ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" href="../promo/">Promo <i class="fas fa-tag"></i></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link p-2 p-md-2 mt-2 mt-md-0" href="../keranjang-saya">
+                        <span class="d-inline d-md-none">Keranjang Saya </span>
+                        <i class="fas fa-shopping-cart position-relative pe-3">
+                            <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"><?= $_SESSION['qty_FamillyFood'] ?></span>
+                        </i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
